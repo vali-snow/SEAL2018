8dc66080996c5348d5df2c36992b6db5bfb740d6
vali-snow <vali-snow@users.noreply.github.com>
2018-11-24 10:11:59 +0200

    trying the SPA clothes on

---
+++ js/route.js
@@ -0,0 +1,30 @@
+/*global console */
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+function Route(name, htmlName, defaultRoute) {
+    'use strict';
+    try {
+        if (!(name && htmlName)) {
+            throw 'error: name and htmlName params are mandatory';
+        }
+        this.constructor(name, htmlName, defaultRoute);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+Route.prototype = {
+    name: undefined,
+    htmlName: undefined,
+    defaultRoute: undefined,
+    constructor: function (name, htmlName, defaultRoute) {
+        'use strict';
+        this.name = name;
+        this.htmlName = htmlName;
+        this.defaultRoute = defaultRoute;
+    },
+    isActiveRoute: function (hashedPath) {
+        'use strict';
+        return hashedPath.replace('#', '') === this.name;
+    }
+};
