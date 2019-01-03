6145d9066240d448af857e70431f3f77f8ff2842
vali-snow <vali-snow@users.noreply.github.com>
2019-01-04 00:47:39 +0200

    first try at push notifications

--- js/app.js
+++ js/app.js
@@ -3,7 +3,52 @@
 
 (function () {
     'use strict';
-    function init() {
+    function initNotification(){
+        var data = {
+            title: 'Test notification',
+            msg: 'Hello world reloaded',
+            msger: 'The one and only',
+            link: 'https://www.google.ro/'
+        }
+        
+        var modal = document.getElementById('notificationModal');
+        var span = document.getElementsByClassName("close")[0];
+        span.onclick = function() {
+            modal.style.display = "none";
+        };
+        var button = document.getElementById('notificationSave');
+        button.onclick = function(){
+            var notificationTypes = {
+                type1: modal.getElementsByTagName("input")[0].checked,
+                type2: modal.getElementsByTagName("input")[1].checked,
+                type3: modal.getElementsByTagName("input")[2].checked
+            };
+            localStorage.setItem("notificationTypes", JSON.stringify(notificationTypes));
+            modal.style.display = "none";
+        }
+        
+        if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function (permission) {
+                if (permission==='granted' && localStorage.getItem('notificationTypes') === null){
+                    modal.style.display = "block";
+                    
+//                    var notification = new Notification(data.title, {
+//                        body: data.msg+ "\n" + data.msger,
+//                        icon: "/img/notify.png",
+//                        timeout: 3000
+//                    });
+//                    notification.onclick = function(event){
+//                        event.preventDefault();
+//                        window.open(data.link, '_blank');
+//                    }
+                }
+                localStorage.removeItem('notificationTypes');
+            });
+        }        
+    }
+    initNotification();
+    
+    function initRouter() {
         var router = new Router([
             new Route('main', 'main.html', true),
             new Route('results', 'results.html'),
@@ -11,5 +56,5 @@
             new Route('report', 'report.html')
         ]);
     }
-    init();
+    initRouter();
 }());
